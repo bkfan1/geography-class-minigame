c3453090ca6db8e7995d3fee6433dc52739611cd
bkfan1 <blossomsc15@gmail.com>
2022-06-05 15:59:35 -0400

    Created component, added some styling

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <h1 style={{ marginTop: "20px", marginBottom: "0px" }}>🌐</h1>
+        <h3 style={{ marginTop: "8px" }}>Geography Class Minigame</h3>
+        <h5>Created by Jackson Paredes Ferranti</h5>
+        <ul>
+          <a title="Github profile" href="https://www.github.com/bkfan1">
+            <i className="bi bi-github" />
+          </a>
+          <a title="Send email" href="mailto:jacksonpf177@gmail.com">
+            <i className="bi bi-envelope-fill" />
+          </a>
+        </ul>
+      </footer>
+    </>
+  );
+}
