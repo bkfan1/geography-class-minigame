cf82eb9175612adf9442d451e95e113cc146af98
bkfan1 <blossomsc15@gmail.com>
2022-06-05 16:01:14 -0400

    Removed conditional rendering and some unused imports

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,19 +1,15 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import { GameContext } from "../context/GameContext";
 
 export default function Timer() {
-  const { matchDraggableFlags, minutes, seconds } = useContext(GameContext);
+  const { minutes, seconds } = useContext(GameContext);
 
   return (
     <div className="timer">
-      {minutes === 0 && seconds === 0 ? null : (
-        <>
-          <h1 className="timerTitle">Time Left</h1>
-          <h1 className={`timerNumbers ${minutes <= 0 ? "warning" : ""}`}>
-            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-          </h1>
-        </>
-      )}
+      <h1 className="timerTitle">Time Left</h1>
+      <h1 className={`timerNumbers ${minutes <= 0 ? "warning" : ""}`}>
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </h1>
     </div>
   );
 }
